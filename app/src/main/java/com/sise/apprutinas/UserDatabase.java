0fd1614c095086f2c6c4c1fb7c92e66127cb07f5
JoelAndreAnyoza <joelanyoza2004@gmail.com>
2026-06-28 21:26:43 -0500

    Modulo de login y registro (con memoria RAM) + simulacion de home

---
+++ app/src/main/java/com/sise/apprutinas/UserDatabase.java
@@ -0,0 +1,25 @@
+package com.sise.apprutinas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDatabase {
+
+    public static List<User> users = new ArrayList<>();
+
+    static {
+        users.add(new User("Admin", "premium@test.com", "123456", "PREMIUM"));
+        users.add(new User("Usuario", "gratuito@test.com", "123456", "GRATUITO"));
+    }
+
+    public static class User {
+        public String name, email, password, type;
+
+        public User(String name, String email, String password, String type) {
+            this.name = name;
+            this.email = email;
+            this.password = password;
+            this.type = type;
+        }
+    }
+}
